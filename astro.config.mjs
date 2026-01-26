880eb4e1cae44d1602b84592380daee6fb897169
mandarin <mandarinyx@gmail.com>
2026-01-26 22:26:54 +0300

    0.1 | поехали

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { webcore } from "webcoreui/integration";
+
+//import node from "@astrojs/node";
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // output: "server",
+  output: "static",
+  //site: "",
+  integrations: [webcore(), purgecss()],
+  //adapter: node({ mode: "standalone" }),
+  build: {
+    // запрещаем встраивать стили в html
+    // https://docs.astro.build/en/guides/styling/#bundle-control
+    inlineStylesheets: "never",
+  },
+});
